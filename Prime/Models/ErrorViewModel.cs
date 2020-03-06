09d04c89fa1ff4ecd6b73546be7e892dcdc41a9f
John Bennett <john.d.bennett@hotmail.com>
2020-03-06 08:25:34 -0600

    Added in a sample project.

---
+++ Prime/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Prime.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
